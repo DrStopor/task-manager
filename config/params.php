f7fa8b385f736dcfaa89bb77c955482482cbd2a4
d.rudov <rds18@mail.ru>
2023-12-17 21:29:53 +0300

    set-env захардкожены настройки подключения к бд замеенны на значения из окружения

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'adminEmail' => getenv('ADMIN_EMAIL')?? 'admin@test.test',
+    'senderEmail' => getenv('SENDER_EMAIL')?? 'noreply@test.test',
+    'senderName' => getenv('SENDER_NAME') ?? 'Task System',
 ];
